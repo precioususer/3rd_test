276e0e23b1382d9e73aeabc1819a97e361e5210f
Valery <valerygusev2022@gmail.com>
2023-04-18 03:46:46 +0300

    finish modals for mobile version

--- js/index.js
+++ js/index.js
@@ -1,13 +1,45 @@
 const navContainer = document.querySelector(".nav-container320_360");
+const callContainer = document.querySelector(".order-call-container320_360");
+const feedbackContainer = document.querySelector(".feedback-container320_360");
+
+// ----- OPEN/CLOSE NAVIGATION -----
 
 document.addEventListener("click", (e) => {
-  if (e.target.closest(".burger").className === "burger") {
+  if (e.target.id === "menu-btn") {
     navContainer.style.display = "flex";
-  } else;
+  }
 });
 
 document.addEventListener("click", (e) => {
-  if (e.target.closest(".nav-close-btn").className === "nav-close-btn") {
+  if (e.target.id === "nav-close-btn") {
     navContainer.style.display = "none";
-  } else;
+  }
+});
+
+// ----- OPEN/CLOSE CALL ORDER -----
+
+document.addEventListener("click", (e) => {
+  if (e.target.id === "call-btn") {
+    callContainer.style.display = "flex";
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target.id === "modal-close-btn") {
+    callContainer.style.display = "none";
+  }
+});
+
+// ----- OPEN/CLOSE FEEDBACK -----
+
+document.addEventListener("click", (e) => {
+  if (e.target.id === "feedback-btn") {
+    feedbackContainer.style.display = "flex";
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target.id === "modal-close-btn") {
+    feedbackContainer.style.display = "none";
+  }
 });
