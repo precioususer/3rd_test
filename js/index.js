83349f51bb06c7474a88f5972d22fc07688c98b4
Valery <valerygusev2022@gmail.com>
2023-05-01 03:46:38 +0300

    start desktop version

--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
 const navContainer = document.querySelector(".nav-container320_360");
 const callContainer = document.querySelector(".order-call-container320_360");
 const feedbackContainer = document.querySelector(".feedback-container320_360");
+const modalBackground = document.querySelector(".modal-bc");
+const navBackground = document.querySelector(".nav-bc");
 
 // ----- OPEN/CLOSE NAVIGATION -----
 
 document.addEventListener("click", (e) => {
   if (e.target.id === "menu-btn") {
     navContainer.style.display = "flex";
+    navBackground.style.display = "flex";
   }
 });
 
 document.addEventListener("click", (e) => {
   if (e.target.id === "nav-close-btn") {
     navContainer.style.display = "none";
+    navBackground.style.display = "none";
   }
 });
 
@@ -21,12 +25,14 @@ document.addEventListener("click", (e) => {
 document.addEventListener("click", (e) => {
   if (e.target.id === "call-btn") {
     callContainer.style.display = "flex";
+    modalBackground.style.display = "flex";
   }
 });
 
 document.addEventListener("click", (e) => {
   if (e.target.id === "modal-close-btn") {
     callContainer.style.display = "none";
+    modalBackground.style.display = "none";
   }
 });
 
@@ -35,11 +41,13 @@ document.addEventListener("click", (e) => {
 document.addEventListener("click", (e) => {
   if (e.target.id === "feedback-btn") {
     feedbackContainer.style.display = "flex";
+    modalBackground.style.display = "flex";
   }
 });
 
 document.addEventListener("click", (e) => {
   if (e.target.id === "modal-close-btn") {
     feedbackContainer.style.display = "none";
+    modalBackground.style.display = "none";
   }
 });
